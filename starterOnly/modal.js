f75195b909fbaa3f4bd4b0262248e3db97345f5c
Duong Trinh <khanhduong.dav@gmail.com>
2023-09-03 23:20:12 +0200

    fix responsive design based on the old version with display grid template

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,3 +26,10 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+//add event close modal to btn Close 
+btnClose.addEventListener('click', closeModal);
+
+//close modal with escape on keyboard
+window.addEventListener('keydown', (e) => {
+  if(e.key === 'Escape') closeModal();
+})
